26c7e15bf7b580994b61ae953f86659b500e344f
Siphiwo10 <siphiwoj10@gmail.com>
2024-11-06 11:57:39 +0000

    leu

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,43 +1,48 @@
-#!/usr/bin/env python3
-"""inherits from BaseCaching"""
-
+#!/usr/bin/python3
+"""LRU Caching System"""
 
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """
-    mainn class for base cashing
+    """LRUCache class that inherits from BaseCaching and implements
+    a caching system with an LRU (Least Recently Used) eviction policy.
     """
 
     def __init__(self):
+        """Initialize the LRUCache class."""
         super().__init__()
-        self.lru_queue = []
+        self.lru_order = []
 
     def put(self, key, item):
+        """Add an item in the cache.
+        
+        If the cache exceeds the MAX_ITEMS, it discards the least
+        recently used item.
+        """
         if key is None or item is None:
             return
 
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            self.__evict_lru()
+        if key in self.cache_data:
+            # Move the key to the end of the LRU order to mark it as recently used
+            self.lru_order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # If cache is full, remove the least recently used item
+            lru_key = self.lru_order.pop(0)
+            del self.cache_data[lru_key]
+            print(f"DISCARD: {lru_key}")
 
+        # Add or update the cache and LRU order
         self.cache_data[key] = item
-        self.__update_lru(key)
+        self.lru_order.append(key)
 
     def get(self, key):
+        """Get an item by key. If the key exists, it is marked as recently used."""
         if key is None or key not in self.cache_data:
             return None
 
-        self.__update_lru(key)
+        # Move accessed key to the end of LRU order
+        self.lru_order.remove(key)
+        self.lru_order.append(key)
         return self.cache_data[key]
 
-    def __update_lru(self, key):
-        if key in self.lru_queue:
-            self.lru_queue.remove(key)
-        self.lru_queue.append(key)
-
-    def __evict_lru(self):
-        if self.lru_queue:
-            lru_key = self.lru_queue.pop(0)
-            del self.cache_data[lru_key]
-            print("DISCARD:", lru_key)
